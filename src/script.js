0b71a505c60b78496f09e2b646a2df757c84e2ca
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-11-22 13:25:41 -0800

    style: code formatting.

--- src/script.js
+++ src/script.js
@@ -11,19 +11,18 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-        const {latitude} = position.coords;
-        const {longitude} =position.coords;
-        const location= `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu&g_ep=EgoyMDI0MTExOS4yIKXMDSoASAFQAw%3D%3D`
-        console.log(location);
-      },
-      (errorMsg) => {
-        console.log(errorMsg);
-        alert("Could not get your location");
-      }
-    );
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const location = `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu&g_ep=EgoyMDI0MTExOS4yIKXMDSoASAFQAw%3D%3D`;
+      console.log(location);
+    },
+    (errorMsg) => {
+      console.log(errorMsg);
+      alert("Could not get your location");
     }
+  );
+}
